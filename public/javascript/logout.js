2c25630f9684a8011d2a0815be51f7e2daff25b9
mukukuu <qinii@me.com>
2022-02-09 20:46:07 -0700

    login page

--- public/javascript/logout.js
+++
@@ -1,7 +0,0 @@
-//user login
-async function loginForm(event) {
-    event.preventDefault();
-
-    const email = dodument.querySelector('#user-email').value.trim();
-    const password = document.querySelector('#user-Passcode').value.trim();
-}
