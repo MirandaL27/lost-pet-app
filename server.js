ef3ef5ac478ed6939a08f29df873678980b337d5
mukukuu <qinii@me.com>
2022-02-12 10:22:24 -0700

    update

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+
 const path = require('path');
 const exphbs = require('express-handlebars');
 const multer  = require('multer')
 //const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ });
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 
 const session = require('express-session');
 
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -29,25 +27,23 @@ const sess = {
 
 };
 
-//----------------upload image
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
-const upload = multer({ storage: storage })
-
-
 app.use(session(sess));
 
 
 
+
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 app.use('/uploads', express.static('uploads'));
 
 // turn on routes
