61d15503dd36feb17151b5b19af4df391b64c8fe
Cadenza <mirandalucsb@gmail.com>
2022-02-12 19:23:09 -0700

    added the ability to post comments to the single pet screen

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -129,6 +129,11 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/search', (req, res) => {
+  //use url params to query the database based on user filter input
+  
+})
+
 
 
 module.exports = router;
