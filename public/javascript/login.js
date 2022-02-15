d83918a0528293235e719ae08d6f71d35f68194e
mukukuu <qinii@me.com>
2022-02-14 17:37:08 -0700

    created partials folder for dashboard, login, petimage, signup and separateted login and signup js files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -23,34 +23,6 @@ async function loginForm(event) {
     }
 }
 
-//sign up form
-async function signupForm(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response =  await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-      //check response status
-      if (response.ok) {
-         document.location.replace('/');
-      } else {
-          alert(response.statusText);
-      }
-    }
-  }
-
 
 
 document.querySelector('.login-form').addEventListener('submit', loginForm);
-document.querySelector('.signup-form').addEventListener('submit', signupForm);
